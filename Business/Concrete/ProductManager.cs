8bc881fa9a2afabf1fc635211d04022450d35c8f
AYBÜKE <AYBKE@DESKTOP-85KUE8U>
2021-12-24 16:39:56 +0300

    24 aralık 2

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Abstract;
+using Business.Constants;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -23,19 +24,20 @@ namespace Business.Concrete
 			//business code
 			if(product.ProductName.Length<2)
 			{
-				return new ErrorResult("Ürün ismi en az 2 karakter olmalıdır");
+				//magic strings
+				return new ErrorResult(Messages.ProductNameInvalid);
 
 			}
 			_productDal.Add(product);
 
-			return new SuccessResult("Ürün eklendi");
+			return new SuccessResult(Messages.ProductAdded);
 		}
 
-		public List<Product> GetAll()
+		public IDataResult<List<Product>> GetAll()
 		{
 			//iş kodları 
 			//yetkisi var mı
-			return _productDal.GetAll();
+			return new SuccessDataResult<List<Product>>( _productDal.GetAll(),true,"Ürünler listelendi");
 		}
 
 		public List<Product> GetAllByCategoryId(int id)
