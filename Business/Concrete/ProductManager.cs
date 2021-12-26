c56de58beef4e188a211c16c1edc21c6c40058b9
AYBÜKE <AYBKE@DESKTOP-85KUE8U>
2021-12-26 22:35:54 +0300

    26 aralık ders sonu

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -37,11 +37,11 @@ namespace Business.Concrete
 		{
 			//iş kodları 
 			//yetkisi var mı
-			//if(DateTime.Now.Hour==22)
-			//{
-				//return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
-			//}
-			
+			if (DateTime.Now.Hour == 2)
+			{
+				return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
+			}
+
 			return new SuccessDataResult<List<Product>>( _productDal.GetAll(),Messages.ProductsListed);
 		}
 
