c45d60572b58396f2581c996471e0011710f75b3
AYBÜKE <AYBKE@DESKTOP-85KUE8U>
2022-01-21 15:50:12 +0300

    21 ocak

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -1,23 +1,9 @@
-using Business.Abstract;
-using Business.Concrete;
-using Core.DependencyResolvers;
-using Core.Extensions;
-using Core.Utilities.IoC;
-using Core.Utilities.Security.Encryption;
-using Core.Utilities.Security.JWT;
-using DataAccess.Abstract;
-using DataAccess.Concrete.EntityFramework;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -37,35 +23,7 @@ namespace WebAPI
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-			//Autofac, Ninject, CastleWindsor, StructureMap, LightInject, DryInject
-			//AOP
-
-			services.AddControllers();
-			//services.AddSingleton<IProductService,ProductManager>();
-			//services.AddSingleton<IProductDal,EfProductDal>();
-
-			services.AddCors();
-			
-			var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
-
-			services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-				.AddJwtBearer(options =>
-				{
-					options.TokenValidationParameters = new TokenValidationParameters
-					{
-						ValidateIssuer = true,
-						ValidateAudience = true,
-						ValidateLifetime = true,
-						ValidIssuer = tokenOptions.Issuer,
-						ValidAudience = tokenOptions.Audience,
-						ValidateIssuerSigningKey = true,
-						IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey)
-					};
-				});
-			services.AddDependencyResolvers(new ICoreModule[]
-			{
-				new CoreModule()
-			});
+			services.AddRazorPages();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -75,21 +33,23 @@ namespace WebAPI
 			{
 				app.UseDeveloperExceptionPage();
 			}
-			app.ConfigureCustomExceptionMiddleware();
-
-			app.UseCors(builder=>builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
+			else
+			{
+				app.UseExceptionHandler("/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
 
 			app.UseHttpsRedirection();
+			app.UseStaticFiles();
 
 			app.UseRouting();
-
-			app.UseAuthentication();
 
 			app.UseAuthorization();
 
 			app.UseEndpoints(endpoints =>
 			{
-				endpoints.MapControllers();
+				endpoints.MapRazorPages();
 			});
 		}
 	}
